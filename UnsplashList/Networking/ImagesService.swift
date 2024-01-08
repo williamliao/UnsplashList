e8e049a92d398ed470e0edee94d4090ac3766bdf
william.liao <william.liao@mitake.com.tw>
2024-01-08 17:40:14 +0800

    update imagesService for endpoint

--- UnsplashList/Networking/ImagesService.swift
+++ UnsplashList/Networking/ImagesService.swift
@@ -22,7 +22,8 @@ class ImagesService: NetworkManager {
 
 extension ImagesService {
     
-    func fetchUnsplash(completion: @escaping (APIResult<[UnsplashModel], ServerError>) -> Void) {
+    func fetchUnsplash<K, R>(for endpoint: Endpoint<K, R>,
+                             using requestData: K.RequestData, completion: @escaping (APIResult<Any, ServerError>) -> Void) {
         
         guard loadingTask == nil else {
             return
@@ -30,13 +31,47 @@ extension ImagesService {
         
         loadingTask = Task {
             
-            let result = try await self.data(for: .random(with: "10"), using: (), decodingType: [UnsplashModel].self)
-            
-            switch result {
-            case .success(let models):
-                completion(.success(models))
-            case .failure(let error):
-                completion(.failure(error as! ServerError))
+            switch endpoint.path {
+                
+                case "photos/random":
+                   let result = try await self.data(for: endpoint, using: requestData, decodingType: [RandomResponse].self)
+                
+                    switch result {
+                    case .success(let models):
+                        
+                        var newModels = [UnsplashModel]()
+                        
+                        for res in models {
+                            let model = UnsplashModel(id: res.id, urls: res.urls, user: res.user, exif: res.exif, location: res.location)
+                            newModels.append(model)
+                        }
+                        
+                        completion(.success(newModels))
+                        
+                    case .failure(let error):
+                        completion(.failure(error as! ServerError))
+                    }
+                
+                case "search/photos":
+                    let result = try await self.data(for: endpoint, using: requestData, decodingType: SearchRespone.self)
+                
+                    switch result {
+                    case .success(let models):
+                        
+                        var newModels = [UnsplashModel]()
+                        
+                        for res in models.results {
+                            let model = UnsplashModel(id: res.id, urls: res.urls, user: res.user, exif: nil, location: nil)
+                            newModels.append(model)
+                        }
+                        
+                        completion(.success(newModels))
+                        
+                    case .failure(let error):
+                        completion(.failure(error as! ServerError))
+                    }
+                default:
+                    break
             }
         }
         
