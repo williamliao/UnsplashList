1ec6789e49e9c80d6c05f396241dec3787fa09e2
william.liao <william.liao@mitake.com.tw>
2024-01-08 17:41:09 +0800

    update gridview

--- UnsplashList/View/GridView.swift
+++ UnsplashList/View/GridView.swift
@@ -11,49 +11,58 @@ struct GridView: View {
     
     @ObservedObject var viewModel: GridViewModel
     @Binding var navigationPath: [Route]
-
+    
     var body: some View {
         
         ScrollView {
             LazyVGrid(columns: [.init(.adaptive(minimum: 200, maximum: .infinity), spacing: 3)], spacing: 3) {
-                ForEach(0 ..< viewModel.items.count, id: \.self) { i in
-                    
-                    AsyncImage(url: URL(string: viewModel.items[i].urls.small)) { phase in
-                        switch phase {
-                        case .empty:
-                            ProgressView()
-                        case .success(let image):
-                            image
-                                    .resizable()
-                                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
-                                    .aspectRatio(1, contentMode: .fit)
-                                    .onTapGesture {
-                                        viewModel.open(model: viewModel.items[i])
-                                        navigationPath.append(.detail)
-                                    }
-                        case .failure(_):
-                            Image(systemName: "wifi.exclamationmark")
-                                .resizable()
-                                .scaledToFit()
-                        @unknown default:
-                            Image(systemName: "wifi.exclamationmark")
-                                .resizable()
-                                .scaledToFit()
-                        }
-                    }
-                    
-                }
-                .animation(.interactiveSpring(), value: 3)
+                
+                showGridView(viewModel.items)
+                
             }
             .padding(.all, 10)
             .task {
                 await MainActor.run {
-                    viewModel.loadData { result in
-                        
+                    if viewModel.items.count == 0 {
+                        viewModel.loadData()
                     }
                 }
             }
         }
+    }
+    
+    func showGridView(_ array: Array<Any>) -> some View {
+        
+        return ForEach(0 ..< array.count, id: \.self) { i in
+            
+            let url = viewModel.items[i].urls?.small
+            
+            AsyncImage(url: URL(string: url!)) { phase in
+                switch phase {
+                case .empty:
+                    ProgressView()
+                case .success(let image):
+                    image
+                            .resizable()
+                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                            .aspectRatio(1, contentMode: .fit)
+                            .onTapGesture {
+                                viewModel.open(model: viewModel.isSearch ? viewModel.items[i] : viewModel.items[i])
+                                navigationPath.append(.detail)
+                            }
+                case .failure(_):
+                    Image(systemName: "wifi.exclamationmark")
+                        .resizable()
+                        .scaledToFit()
+                @unknown default:
+                    Image(systemName: "wifi.exclamationmark")
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+            
+        }
+        .animation(.interactiveSpring(), value: 3)
     }
 }
 
