35b2cb3234e5938e5fdfec4da69b015c06410b7d
william.liao <william.liao@mitake.com.tw>
2024-05-29 14:51:58 +0800

    update webview

--- UnsplashList/Share/SafariWebView.swift
+++ UnsplashList/Share/SafariWebView.swift
@@ -6,11 +6,12 @@
 //
 
 import SwiftUI
+import WebKit
 
+#if canImport(SafariServices)
 #if canImport(UIKit)
 import UIKit
 import SafariServices
-import WebKit
 
 struct SafariWebView: UIViewControllerRepresentable {
     let url: URL
@@ -23,27 +24,27 @@ struct SafariWebView: UIViewControllerRepresentable {
         
     }
 }
+#endif
+#endif
 
-struct WebView: UIViewRepresentable {
+struct WebView {
  
     let url: URL
     @Binding var isLoading: Bool
     @Binding var error: Error?
     
-    func makeCoordinator() -> Coordinator {
+    func makeCoordinator() -> WebView.Coordinator {
         Coordinator(self)
     }
     
-    func makeUIView(context: Context) -> WKWebView  {
-        let wkwebView = WKWebView()
-        //wkwebView = context.coordinator
-        wkwebView.load(URLRequest(url: url))
-        return wkwebView
+    func makeWebView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        webView.navigationDelegate = context.coordinator
+        let request = URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad)
+        webView.load(request)
+        return webView
     }
-    
-    func updateUIView(_ uiView: WKWebView, context: Context) {
-    }
-
+  
     class Coordinator: NSObject, WKNavigationDelegate {
         var parent: WebView
         init(_ parent: WebView) {
@@ -65,45 +66,23 @@ struct WebView: UIViewRepresentable {
         }
     }
 }
-#elseif canImport(Cocoa)
-import WebKit
-struct WebView: NSViewRepresentable {
-    let url: URL
-    @Binding var isLoading: Bool
-    @Binding var error: Error?
-    
-    func makeCoordinator() -> Coordinator {
-        Coordinator(self)
+
+#if os(macOS)
+extension WebView: NSViewRepresentable {
+    func makeNSView(context: Context) -> WKWebView {
+        makeWebView(context: context)
     }
-    
-    func makeNSView(context: Context) -> WKWebView  {
-        var wkwebView = WKWebView()
-       // wkwebView = context.coordinator
-        wkwebView.load(URLRequest(url: url))
-        return wkwebView
-    }
-    
     func updateNSView(_ nsView: WKWebView, context: Context) {
-    }
 
-    class Coordinator: NSObject, WKNavigationDelegate {
-        var parent: WebView
-        init(_ parent: WebView) {
-            self.parent = parent
-        }
-        func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-            parent.isLoading = true
-        }
-        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-            parent.isLoading = false
-        }
-        
-        func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
-            print("loading error: \(error)")
-            parent.isLoading = false
-            parent.error = error
-        }
     }
 }
+#else
+extension WebView: UIViewRepresentable {
+     func makeUIView(context: Context) -> WKWebView {
+         makeWebView(context: context)
+     }
 
+     func updateUIView(_ uiView: WKWebView, context: Context) {
+     }
+}
 #endif
